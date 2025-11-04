ff2f913428721b67ee626b3dfe546555fff90165
yatarasov724 <maybeowned@gmail.com>
2025-11-04 19:22:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const auditApi = require('./pages/api/audit');
+
 const app = express();
 const port = 3000;
 
+// Роут для проверки
 app.get('/', (req, res) => {
   res.send('UX Audit Bot is running');
 });
 
+// Подключение ручки audit
+app.get('/api/audit', auditApi);
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`✅ Server is running at http://localhost:${port}`);
 });
